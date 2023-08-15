de4bdf895e71a2b1a9e8f518b90725659765311c
wuqinfa <369724377@qq.com>
2023-08-15 21:01:02 +0800

    chore: init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2023-08-11 20:15:34
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
 import type { IApi } from 'umi';
+
+import { KEY, } from './interface/const';
 
 export default (api: IApi) => {
   // See https://umijs.org/docs/guides/plugins
+  api.describe({
+    key: KEY,
+    config: {
+      schema(joi) {
+        return joi.string();
+      },
+    },
+    enableBy: api.EnableBy.config
+  });
+
+  api.onBuildComplete(async ({ err }) => {
+
+  });
+
+  api.modifyConfig((initValue) => {
+    const { publicPath } = initValue || {};
+    if (api.userConfig[KEY].oss && (publicPath === '/' || publicPath === '')) {
+        api.logger.warn(`❗️  请检查是否正确配置publicPath,未正确配置将导致HTML文件无法使用阿里云OSS文件`);
+        api.logger.warn(`❗️  配置示例：https://umi-test.oss-cn-hangzhou.aliyuncs.com/umi-test/`);
+    }
+    return initValue;
+  });
 };
---
+++ src/interface/const.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2023-08-15 20:45:48
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
+export const KEY = 'qiniu';
